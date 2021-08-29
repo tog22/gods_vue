840f25b068dbd5f082dca195250e333356bfdfa2
tog22 <tom.ash.github@philosofiles.com>
2021-08-29 15:20:36 -0700

    Styles

--- App.vue
+++ App.vue
@@ -16,11 +16,5 @@ export default {
 </script>
 
 <style>
-#app {
-  max-width: 400px;
-  
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+
 </style>
