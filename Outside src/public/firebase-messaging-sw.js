c3125f6722fb3f3fd0e9732d40a56cb7fa50f8e0
tog22 <tom.ash.github@philosofiles.com>
2022-02-17 16:28:43 -0800

    firebase-messaging-sw.js: Version from pre-Feb try

---
+++ Outside src/public/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+// Version from pre-Feb try
+
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGhSRGXLRM1m-nNMFNuJnSKu5AX--6vb0",
+  authDomain: "godsgamefbase.firebaseapp.com",
+  projectId: "godsgamefbase",
+  storageBucket: "godsgamefbase.appspot.com",
+  messagingSenderId: "306649763697",
+  appId: "1:306649763697:web:228785d43cabe34913b0d0"
+};
+
+const app = firebase.initializeApp(firebaseConfig)
